5b76bcc2e553f391ebbabe8728c8c27ac4e997fd
Amer Salah Ali <asalah@citglobal.com>
2019-06-23 16:22:12 +0200

    interact with another webservice using WebClient instead if RestTemplate

--- movie-catalog-service/movie-catalog-service/src/main/java/com/amer/moviecatalogservice/controller/CatalogController.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/amer/moviecatalogservice/controller/CatalogController.java
@@ -4,10 +4,13 @@ import com.amer.moviecatalogservice.dal.model.CatalogItem;
 import com.amer.moviecatalogservice.dal.model.Movie;
 import com.amer.moviecatalogservice.dal.model.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,17 +22,27 @@ public class CatalogController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder builder;
+
+    //private WebClient.Builder builder =  WebClient.builder();
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
 
         List<Rating> ratings = Arrays.asList(
-            new Rating(1234 , 4),
-            new Rating(5678 , 3)
+            new Rating(5678 , 4),
+            new Rating(1234 , 3)
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId() , Movie.class);
+           // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId() , Movie.class);
+
+            Movie movie = builder.build().get().uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve().bodyToMono(Movie.class)
+                    .block();
+
             return new CatalogItem(movie.getName() , movie.getDescription() , rating.getRating());
         }).collect(Collectors.toList());
 
